1768b0e98d1825dc094cf9c28e0ab8905c1428ca
Eduardo Souza <eduardo.dasilva@canoastec.rs.gov.br>
2022-12-27 13:34:59 -0300

    modulo de categorias

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -3,5 +3,6 @@ const router = require("express").Router();
 router.use("/usuarios", require("./usuarios"));
 router.use("/lojas", require("./lojas"));
 router.use("/clientes", require("./clientes"));
+router.use("/categorias", require("./categorias"));
 
 module.exports = router;
